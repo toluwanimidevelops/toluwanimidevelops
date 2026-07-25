7cb5790c5c7dfe218ce28783f103c2760ad61fd6
toluwebdev <oladojatolu@gmail.com>
2026-07-25 18:11:37 +0100

    website update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,9 +8,21 @@ const ProjectCardShip = ({ text }) => {
   );
 };
 
-const ProjectCard = ({ img, link, year, title, thing, description, languages }) => {
+const ProjectCard = ({
+  img,
+  link,
+  year,
+  title,
+  thing,
+  description,
+  languages,
+}) => {
   return (
-    <a target="_blank" href={link} className="w-full  group overflow-hidden rounded-3xl bg-gradient-to-b from-[#0f0f10] to-transparent rounded-xl border border-[#b0b0b5]/20 transition-all duration-500 hover:border-blue-700 hover:from-blue-600/10 hover:to-transparent cursor-pointer">
+    <a
+      target="_blank"
+      href={link}
+      className="w-full  group overflow-hidden rounded-3xl bg-gradient-to-b from-[#0f0f10] to-transparent rounded-xl border border-[#b0b0b5]/20 transition-all duration-500 hover:border-blue-700 hover:from-blue-600/10 hover:to-transparent cursor-pointer"
+    >
       <div className="w-full overflow-hidden relative h-[200px]">
         <div className="flex text-[#b0b0b5] absolute z-2 w-full p-2 justify-between">
           <p className="backdrop-blur-sm font-primary uppercase  rounded-full  text-xs leading-none tracking-widest flex justify-center items-center p-2 px-3 bg-black/60">
@@ -26,18 +38,18 @@ const ProjectCard = ({ img, link, year, title, thing, description, languages }) 
           className="w-full duration-500 h-full group-hover:scale-[1.07] object-cover"
         />
       </div>
-      <div className="w-full  p-7">
-        <h1 className="text-xl pb-2 font-secondary flex justify-between text-white">
+      <div className="w-full max-md:p-5 max-sm:p-4  p-7">
+        <h1 className="text-xl max-sm:text-lg max-md:pb-2 pb-2 font-secondary flex justify-between text-white">
           {title}{" "}
           <MdArrowOutward
             className="group-hover:text-blue-700 group-hover:-translate-y-[5px] group-hover:translate-x-[5px] duration-500"
             size={15}
           />
         </h1>
-        <p className="font-main text-[#b0b0b5] text-[14px] pb-5 ">
+        <p className="font-main text-[#b0b0b5] text-[14px] max-sm:text-[12px] pb-5 max-md:pb-2">
           {description}
         </p>
-        <div className="flex gap-1">
+        <div className="flex flex-wrap gap-1">
           {languages?.map((e, index) => (
             <ProjectCardShip key={index} text={e} />
           ))}
@@ -73,7 +85,7 @@ const Project = () => {
           </p>
         </div>
       </header>
-      <div className="lg:grid-cols-2 my-15 gap-4 grid-cols-1 grid">
+      <div className="md:grid-cols-2 my-15 gap-4 grid-cols-1 grid">
         <ProjectCard
           img={"/encounterroom.png"}
           year={"2026"}
@@ -96,7 +108,7 @@ const Project = () => {
           }
           languages={["React", "Mongo Db", "SMTP", "Express.js", "Node.js"]}
         />
-       
+
         <ProjectCard
           img={"/guest'n'host.png"}
           year={"2026"}
@@ -108,7 +120,14 @@ const Project = () => {
           }
           languages={["HTML 5", "Tailwind Css"]}
         />
+        <a
+          className="text-center max-md:col-span-1 col-span-2 text-[#b0b0b5]/50 text-primary uppercase "
+          href="https://github.com/toluwanimidevelops"
+        >
+          View more on my github
+        </a>
       </div>
+
       {/* Content Section */}
     </div>
   );
